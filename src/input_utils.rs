a69db314c52a09666544f9ff78e3ea977802b38d
KWeaver87 <28742312+KWeaver87@users.noreply.github.com>
2020-12-01 20:03:30 -0500

    Adds basic documentation and some cleanup

--- src/input_utils.rs
+++ src/input_utils.rs
@@ -4,19 +4,19 @@ use std::{
     fs::{self},
 };
 
-// inputName should be formatted as "dayX-X"
+/// Loads an input file as a `Vec<usize>`
+/// Formatted as `dayX-X`, e.g. `day1-1`
 #[allow(dead_code)]
 pub fn load_as_vec_usize(input_name: &str) -> Vec<usize> {
-    let err_msg = "failure at ";
     let inputs: Vec<&str> = input_name.split_terminator("-").collect();
-    let mut path = env::current_dir().expect(&[err_msg, "current_dir"].concat());
+    let mut path = env::current_dir().expect("failure at current_dir");
     path.push("inputs");
     path.push(inputs[0]);
     path.push(inputs[1]);
-    let contents = fs::read_to_string(&path).expect(path.to_str().unwrap_or_default());
+    let contents = fs::read_to_string(&path).expect(&path.to_str().unwrap_or_default());
     let content_lines = contents.lines();
 
     content_lines
-        .map(|l| usize::from_str(l).expect(l))
+        .map(|line| usize::from_str(line).expect(line))
         .collect()
 }
