a9c9086f43b7d5b077b1536a04c3c6e6c2dbe712
KWeaver87 <28742312+KWeaver87@users.noreply.github.com>
2020-12-01 21:32:01 -0500

    Adds multiply_2020_from_three for second puzzle

--- src/expense_report.rs
+++ src/expense_report.rs
@@ -2,9 +2,27 @@
 #[allow(dead_code)]
 fn multiply_2020_from_two(report: &Vec<usize>) -> usize {
     for a in report {
-        let found = report.into_iter().find(|&b| a + b == 2020);
-        if found.is_some(){
-            return a * found.unwrap();
+        let maybe_b = report.into_iter().find(|&b| a + b == 2020);
+        if maybe_b.is_some() {
+            return a * maybe_b.unwrap();
+        }
+    }
+
+    panic!("Could not find 2020.");
+}
+
+/// Find three entries that sum to 2020 and then multiply those two numbers together
+#[allow(dead_code)]
+fn multiply_2020_from_three(report: &Vec<usize>) -> usize {
+    let length = report.len() - 1;
+    for a_index in 0..=length {
+        let a = report[a_index];
+        for b_index in a_index + 1..=length {
+            let b = report[b_index];
+            let maybe_c = report.into_iter().find(|&c| a + b + c == 2020);
+            if maybe_c.is_some() {
+                return a * b * maybe_c.unwrap();
+            }
         }
     }
 
@@ -30,7 +48,32 @@ mod tests {
 
         let report = &load_as_vec_usize("day1-1");
         let actual = multiply_2020_from_two(report);
-        println!("{}{}", "Product of 2020 entries (2): ".green().bold(), actual);
+        println!(
+            "{}{}",
+            "Product of 2020 entries (2): ".green().bold(),
+            actual
+        );
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn given_example_for_three() {
+        let test_report = &vec![1721, 979, 366, 299, 675, 1456];
+        assert_eq!(multiply_2020_from_three(test_report), 241861950);
+    }
+
+    #[test]
+    fn run_input_for_three() {
+        let expected = 145245270;
+
+        let report = &load_as_vec_usize("day1-1");
+        let actual = multiply_2020_from_three(report);
+        println!(
+            "{}{}",
+            "Product of 2020 entries (3): ".green().bold(),
+            actual
+        );
 
         assert_eq!(actual, expected);
     }
