32978311135372968344c7ef1806fd8596c49282
kweaver87 <28742312+KWeaver87@users.noreply.github.com>
2020-12-05 18:46:07 -0500

    Adds load_as_string function

--- src/input_utils.rs
+++ src/input_utils.rs
@@ -16,10 +16,14 @@ pub fn load_as_vec_usize(input_name: &str) -> Vec<usize> {
 /// Loads an input file as a `Vec<String>`
 /// Formatted as `dayX`, e.g. `day1`
 pub fn load_as_vec_string(input_name: &str) -> Vec<String> {
+    load_as_string(input_name).lines().map(|s| s.into()).collect()
+}
+
+/// Loads an input file as a `String`
+/// Formatted as `dayX`, e.g. `day1`
+pub fn load_as_string(input_name: &str) -> String {
     let mut path = env::current_dir().expect("failure at current_dir");
     path.push("inputs");
     path.push(input_name);
-    let contents = fs::read_to_string(&path).expect(&path.to_str().unwrap());
-
-    contents.lines().map(|s| s.into()).collect()
+    fs::read_to_string(&path).expect(&path.to_str().unwrap())
 }
