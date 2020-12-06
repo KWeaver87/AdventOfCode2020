91728fff4859770f82d9bf189411d203250ab69f
kweaver87 <28742312+KWeaver87@users.noreply.github.com>
2020-12-05 22:54:25 -0500

    Adds part2

--- src/binary_boarding.rs
+++ src/binary_boarding.rs
@@ -1,3 +1,34 @@
+fn find_my_seat(input: Vec<String>) -> usize {
+    let mut ticket_seat_ids: Vec<usize> = input.iter().map(|s| calc_seat_id(s)).collect();
+    ticket_seat_ids.sort();
+
+    let max_seat_id = *ticket_seat_ids.last().unwrap();
+
+    let all_seats: Vec<usize> = (0..max_seat_id).collect();
+
+    let missing_seats: Vec<usize> = all_seats
+        .iter()
+        .filter(|s| !ticket_seat_ids.contains(s))
+        .map(|s| *s)
+        .collect();
+
+    // Find the missing seat that has tickets for adjacent seats
+    let my_seat: Vec<usize> = missing_seats
+        .iter()
+        // We know seat ID 0 isn't right, so skip it to avoid more logic
+        .skip(1)
+        .filter(|s| {
+            ticket_seat_ids.contains(&(*s - 1 as usize))
+                && ticket_seat_ids.contains(&(*s + 1 as usize))
+        })
+        .map(|s| *s)
+        .collect();
+
+    *my_seat
+        .get(0)
+        .unwrap()
+}
+
 fn calc_seat_max_id(input: Vec<String>) -> usize {
     input.iter().map(|s| calc_seat_id(s)).max().unwrap()
 }
@@ -26,7 +57,7 @@ fn split_input(input: String) -> (String, String) {
     let finder: &[_] = &['L', 'R'];
     let split_index = input.find(finder).unwrap();
 
-    let fb = input[0..split_index - 1].to_string();
+    let fb = input[0..split_index].to_string();
     let lr = input[split_index..].to_string();
 
     (fb, lr)
@@ -34,9 +65,9 @@ fn split_input(input: String) -> (String, String) {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use crate::input_utils::load_as_vec_string;
     use colored::Colorize;
-    use super::*;
 
     #[test]
     fn calc_seat_example_1() {
@@ -77,6 +108,17 @@ mod tests {
         let seats = load_as_vec_string("day5");
         let actual = calc_seat_max_id(seats);
         println!("{}{}", "Max seat ID: ".green().bold(), actual);
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn find_my_seat_from_input() {
+        let expected = 515;
+
+        let seats = load_as_vec_string("day5");
+        let actual = find_my_seat(seats);
+        println!("{}{}", "My seat ID: ".green().bold(), actual);
 
         assert_eq!(actual, expected);
     }
