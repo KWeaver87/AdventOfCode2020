764d7715d14f9f9cb9026036214b315c89d8302b
kweaver87 <28742312+KWeaver87@users.noreply.github.com>
2020-12-05 21:19:20 -0500

    Adds working seat id calculator

--- src/binary_boarding.rs
+++ src/binary_boarding.rs
@@ -1,7 +1,31 @@
 fn calc_seat_id(input: &str) -> usize {
+    let rows: Vec<u8> = (0..128).collect();
+    let columns: Vec<u8> = (0..8).collect();
+    let (f_b, l_r) = split_input(input.to_string());
 
+    let seat_partitioner = |a: Vec<u8>, c: char| match c {
+        'F' | 'L' => a[0..(a.len() / 2)].to_vec(),
+        'B' | 'R' => a[(a.len() / 2)..].to_vec(),
+        _ => panic!("Invalid char"),
+    };
 
-    0
+    // Using temp variables to prevent values from being dropped
+    let row_temp = f_b.chars().fold(rows, seat_partitioner);
+    let row = *row_temp.get(0).unwrap() as usize;
+    let col_temp = l_r.chars().fold(columns, seat_partitioner);
+    let col = *col_temp.get(0).unwrap() as usize;
+
+    row * 8 + col
+}
+
+fn split_input(input: String) -> (String, String) {
+    let finder: &[_] = &['L', 'R'];
+    let split_index = input.find(finder).unwrap();
+
+    let fb = input[0..split_index - 1].to_string();
+    let lr = input[split_index..].to_string();
+
+    (fb, lr)
 }
 
 #[cfg(test)]
