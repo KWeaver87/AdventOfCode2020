29a1e5b517b5d4563dd06c88e73732f3a1650b83
KWeaver87 <28742312+KWeaver87@users.noreply.github.com>
2020-12-06 20:25:44 -0500

    Adds function for counting validated passports

--- src/passport_processing.rs
+++ src/passport_processing.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use regex::Regex;
 
 struct ValidatedField<'a> {
@@ -64,6 +66,31 @@ fn passports_with_req_fields(batch: &str) -> usize {
         .count()
 }
 
+/// Gives a count of passwords that contain all required fields and validates their contents
+fn passports_with_valid_fields(batch: &str) -> usize {
+    split_passports_from_batch(batch)
+        .iter()
+        .map(|pp| map_pp_to_hashmap(*pp))
+        .filter(|pp_map| {
+            REQ_VAL_FIELDS
+                .iter()
+                .all(|field| pp_map.contains_key(field.id) && (field.validation)(pp_map[field.id]))
+        })
+        .count()
+}
+
+fn map_pp_to_hashmap(passport: &str) -> HashMap<&str, &str> {
+    let ids_fields = passport.split_whitespace().collect::<Vec<&str>>();
+
+    ids_fields
+        .iter()
+        .map(|f| {
+            let k_v = f.split(':').collect::<Vec<&str>>();
+            (k_v[0], k_v[1])
+        })
+        .collect()
+}
+
 fn split_passports_from_batch(batch: &str) -> Vec<&str> {
     batch.split("\n\n").collect()
 }
@@ -97,11 +124,71 @@ mod tests {
     }
 
     #[test]
-    fn count_passports_with_req_fields_input() {
+    fn count_passports_with_req_fields_from_input() {
         let expected = 245;
 
         let passport_batch = load_as_string("day4");
         let actual = passports_with_req_fields(passport_batch.as_str());
+        println!(
+            "{}{}",
+            "Number of passports with all required fields: "
+                .green()
+                .bold(),
+            actual
+        );
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn count_passports_with_valid_fields_example_1() {
+        let expected = 0;
+        let actual = passports_with_valid_fields(
+            "eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007",
+        );
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn count_passports_with_valid_fields_example_2() {
+        let expected = 4;
+        let actual = passports_with_valid_fields(
+            "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719",
+        );
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn count_passports_with_valid_fields_from_input() {
+        let expected = 133;
+
+        let passport_batch = load_as_string("day4");
+        let actual = passports_with_valid_fields(passport_batch.as_str());
         println!("{}{}", "Number of valid passports: ".green().bold(), actual);
 
         assert_eq!(actual, expected);
