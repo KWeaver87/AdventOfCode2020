59432a5d042ed8a82d47ca3a2bcfe9de017cdc54
KWeaver87 <28742312+KWeaver87@users.noreply.github.com>
2020-12-06 19:15:36 -0500

    Adds validations for each field type

--- src/passport_processing.rs
+++ src/passport_processing.rs
@@ -1,14 +1,65 @@
-static REQUIRED_FIELDS: &[&str] = &["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+use regex::Regex;
 
-fn count_valid_passports(batch: &str) -> usize {
+struct ValidatedField<'a> {
+    id: &'a str,
+    validation: fn(&str) -> bool,
+}
+
+static REQ_VAL_FIELDS: &[ValidatedField] = &[
+    ValidatedField {
+        id: "byr",
+        validation: |v| v >= "1920" && v <= "2002",
+    },
+    ValidatedField {
+        id: "iyr",
+        validation: |v| v >= "2010" && v <= "2020",
+    },
+    ValidatedField {
+        id: "eyr",
+        validation: |v| v >= "2020" && v <= "2030",
+    },
+    ValidatedField {
+        id: "hgt",
+        validation: |v| validate_height(v),
+    },
+    ValidatedField {
+        id: "hcl",
+        validation: |v| validate_hex_color(v),
+    },
+    ValidatedField {
+        id: "ecl",
+        validation: |v| "amb blu brn gry grn hzl oth".contains(v),
+    },
+    ValidatedField {
+        id: "pid",
+        validation: |v| v.len() == 9 && v >= "000000001" && v <= "999999999",
+    },
+];
+
+fn validate_height(value: &str) -> bool {
+    // Get last 2 characters
+    match &value[(value.len() - 2)..] {
+        "cm" => value >= "150cm" && value <= "193cm",
+        "in" => value >= "59in" && value <= "76in",
+        _ => false,
+    }
+}
+
+fn validate_hex_color(value: &str) -> bool {
+    let re = Regex::new(r"#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$").unwrap();
+    re.is_match(value)
+}
+
+/// Gives a count of passwords that contain all required fields.
+fn passports_with_req_fields(batch: &str) -> usize {
     split_passports_from_batch(batch)
         .iter()
         .filter(|pp| {
-            // bind locally so that it can be properly dereferenced
+            // bind here so that it can be dereferenced
             let passport = **pp;
-            REQUIRED_FIELDS
+            REQ_VAL_FIELDS
                 .iter()
-                .all(|field| passport.contains(format!("{}:", field).as_str()))
+                .all(|field| passport.contains(format!("{}:", field.id).as_str()))
         })
         .count()
 }
@@ -33,24 +84,24 @@ iyr:2011 ecl:brn hgt:59in";
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use crate::input_utils::load_as_string;
     use colored::Colorize;
-    use super::*;
 
     #[test]
-    fn count_valid_passwords_example() {
+    fn count_passports_with_req_fields_example() {
         let expected = 2;
-        let actual = count_valid_passports(TEST_PASSPORT_BATCH);
+        let actual = passports_with_req_fields(TEST_PASSPORT_BATCH);
 
         assert_eq!(actual, expected);
     }
 
     #[test]
-    fn count_valid_passwords_input() {
+    fn count_passports_with_req_fields_input() {
         let expected = 245;
 
         let passport_batch = load_as_string("day4");
-        let actual = count_valid_passports(passport_batch.as_str());
+        let actual = passports_with_req_fields(passport_batch.as_str());
         println!("{}{}", "Number of valid passports: ".green().bold(), actual);
 
         assert_eq!(actual, expected);
