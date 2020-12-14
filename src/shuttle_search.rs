05d816cca91a94ef513b3a3ef66816af2f3fdf1b
kweaver87 <28742312+KWeaver87@users.noreply.github.com>
2020-12-13 21:14:01 -0500

    Adds possible solution to Part2
    However, takes a long time (1+ hour) to iterate up to the answer.

--- src/shuttle_search.rs
+++ src/shuttle_search.rs
@@ -1,11 +1,13 @@
+#[derive(PartialEq, Clone)]
 enum BusId {
-    Id(usize),
+    Id(u64),
     NoConstraintsId,
 }
 
-fn multiply_earliest_bus(schedule: String) -> usize {
+/// Part1
+fn multiply_earliest_bus(schedule: String) -> u64 {
     let (depart_time, parsed_sch) = parse_schedule(schedule);
-    let just_constrained_busses: Vec<usize> = parsed_sch
+    let just_constrained_busses: Vec<u64> = parsed_sch
         .into_iter()
         .filter_map(|b| match b {
             BusId::NoConstraintsId => None,
@@ -14,9 +16,9 @@ fn multiply_earliest_bus(schedule: String) -> usize {
         .collect();
 
     let (time, bus) = just_constrained_busses.into_iter().fold(
-        (usize::MAX, 0 as usize),
+        (u64::MAX, 0 as u64),
         |(best_time, best_bus), bus| {
-            let time_till_next = bus - (depart_time % bus);
+            let time_till_next = bus - (depart_time as u64 % bus);
 
             if time_till_next < best_time {
                 (time_till_next, bus)
@@ -27,6 +29,65 @@ fn multiply_earliest_bus(schedule: String) -> usize {
     );
 
     time * bus
+}
+
+fn find_subsequent_time(schedule: String) -> u64 {
+    let (_, parsed_sch) = parse_schedule(schedule);
+    let max_bus = parsed_sch
+        .iter()
+        .map(|bus| match bus {
+            BusId::Id(id) => id,
+            BusId::NoConstraintsId => &0,
+        })
+        .enumerate()
+        .max_by(|x, y| x.1.cmp(y.1))
+        .unwrap();
+    let mut time = 0;
+
+    for n in 1..(u64::MAX / *max_bus.1) {
+    // for n in 1..usize::MAX {
+        time = (max_bus.1 * n) - max_bus.0 as u64;
+        // time = n;
+        // let mut sub_time = time + 1;
+        if time % 1_000_000 == 0 {
+            println!("Checking: {}", time);
+        }
+
+        let x = parsed_sch
+            .iter()
+            // .skip(1)
+            .try_fold(time, |sub_time, bus| match bus {
+                BusId::Id(id) => {
+                    if sub_time % id == 0 {
+                        Ok(sub_time + 1)
+                    } else {
+                        Err(())
+                    }
+                }
+                BusId::NoConstraintsId => Ok(sub_time + 1),
+            });
+
+        if x.is_ok() {
+            break;
+        }
+
+        // for bus in parsed_sch.iter().skip(1) {
+        //     match bus {
+        //         BusId::Id(id) => {
+        //             if sub_time % id == 0 {
+        //                 sub_time += 1;
+        //             } else {
+        //                 break;
+        //             }
+        //         }
+        //         BusId::NoConstraintsId => {
+        //             sub_time += 1;
+        //         }
+        //     }
+        // }
+    }
+
+    time
 }
 
 fn parse_schedule(schedule: String) -> (usize, Vec<BusId>) {
@@ -65,6 +126,7 @@ mod tests {
         assert_eq!(actual, expected);
     }
 
+    // Part1
     #[test]
     fn multiply_earliest_bus_from_input() {
         let expected = 246;
@@ -74,6 +136,50 @@ mod tests {
         println!(
             "{}{}",
             "Product of earliest bus wait time and ID: ".green().bold(),
+            actual
+        );
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn find_subsequent_time_example() {
+        let expected = 1068781;
+        let actual = find_subsequent_time(EXAMPLE_SCHEDULE.to_string());
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn find_subsequent_time_multi_examples() {
+        let examples = vec![
+            ("0\n17,x,13,19", 3417),
+            ("0\n67,7,59,61", 754018),
+            ("0\n67,x,7,59,61", 779210),
+            ("0\n67,7,x,59,61", 1261476),
+            ("0\n1789,37,47,1889", 1202161486),
+        ];
+
+        for ex in examples {
+            let actual = find_subsequent_time(ex.0.to_string());
+
+            assert_eq!(actual, ex.1);
+            println!("Passed for {}", actual);
+        }
+    }
+
+    // Part2
+    #[test]
+    fn find_subsequent_time_from_input() {
+        let expected = 0;
+
+        let schedule = load_as_string("day13");
+        let actual = find_subsequent_time(schedule);
+        println!(
+            "{}{}",
+            "Earliest time where busses arrive at each subsequent minute: "
+                .green()
+                .bold(),
             actual
         );
 
